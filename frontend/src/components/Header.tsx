87a60fca8c294bd784c05f49e49ac7011c0d28ed
Enrique Fresno <sithgto@gmail.com>
2025-11-25 20:57:32 +0100

    Colocado Icono de Torre de gracia

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,10 @@ import Menu from './Menu';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="header-title">Diario de Intimidad</div>
+      <div className="logo-section">
+        <img src="/images/LOGOTIPOPRINCIPAL-SINFONDO.png" alt="Logo Diario de Intimidad" className="logo" />
+        <div className="header-title">Diario de Intimidad</div>
+      </div>
       <Menu />
     </header>
   );
